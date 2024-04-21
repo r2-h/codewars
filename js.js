d856adbc86b63a106d3ddb6962cef726037c9118
Artur Khareksian <r2mail1122@gmail.com>
2024-04-21 09:25:31 +0300

    kk

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+//
+
+
 
 
 
@@ -20,24 +23,13 @@ function deepCopy(obj) {
   return result;
 }
 
-console.log(9)
+
 const tree = {
-  value: 1,
-  children: [
-    {
-      value: 2,
-      children: [
-        {value: 3}
-      ]
-    },
-    {
-      value: 4,
-      children: [
-        {value: 5},
-        {value: 6},
-      ]
-    }
-  ]
+  value: 1, children: [{
+    value: 2, children: [{value: 3}]
+  }, {
+    value: 4, children: [{value: 5}, {value: 6},]
+  }]
 }
 
 // function getTreeValues(tree) {
