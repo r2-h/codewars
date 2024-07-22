13a6c26bc55ea2f0f3750a32054e73064f2e5acc
Artur Khareksian <r2mail1122@gmail.com>
2024-07-22 15:33:29 +0300

    commit

--- js.js
+++ js.js
@@ -156,13 +156,7 @@
 
 //prototype ---------------------------------------------------------------------------------------------------
 
-/* // write methods
-Number.prototype.plus = function (value) {
-  return this + value
-}
-Number.prototype.minus = function (value) {
-  return this - value
-} */
+// write methods
 
 // console.log((2).plus(3).minus(1)) // 4
 
