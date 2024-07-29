fe19f2af4dc86943894aba7a9223a79976732750
Artur Khareksian <r2mail1122@gmail.com>
2024-07-29 13:24:19 +0300

    commit

---
+++ src/s1-main/test/contacts.tsx
@@ -0,0 +1,16 @@
+// Есть два input name и phone. При нажатии на кнопку Add должен добавляться контакт с именем и номером в список контактов
+// Добавить возможность удалять контакты
+
+export function Contacts() {
+  return (
+    <div>
+      <h1>Contacts list</h1>
+      <input />
+      <input />
+      <button>Add</button>
+      <ul>
+
+      </ul>
+    </div>
+  )
+}
