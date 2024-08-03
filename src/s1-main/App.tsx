ff6af101ed29c2597264c0f9f26f04aaef9b6557
Artur Khareksian <r2mail1122@gmail.com>
2024-08-03 15:57:51 +0300

    commit

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,18 +1,10 @@
-import React from "react"
-import { Buttons } from "./test/buttons"
-
-// function App() {
-//   return (
-//     <div className={s.App}>
-//       <HW5 />
-//     </div>
-//   )
-// }
+import s from "./App.module.css"
+import HW5 from "../s2-homeworks/hw05/HW5"
 
 function App() {
   return (
-    <div>
-      <Buttons />
+    <div className={s.App}>
+      <HW5 />
     </div>
   )
 }
--- src/s1-main/test/render1.tsx
+++ src/s1-main/test/render1.tsx
@@ -2,16 +2,18 @@ import { useEffect, useState, FC, PropsWithChildren } from "react"
 
 // Будет ли ререндериться компонент OtherFc?
 const OtherFc = () => {
-  console.log("render")
+  console.log("OtherFc")
 
+  useEffect(() => console.log("OtherFcUseEffect"), [])
   return <div>2</div>
 }
 
 const MainFc: FC<PropsWithChildren> = ({ children }) => {
   const [state, setState] = useState(0)
-  console.log("state", state)
+  console.log("MainFc", state)
 
   useEffect(() => {
+    console.log("MainFcUseEffect")
     setInterval(() => {
       setState((prev) => prev + 1)
     }, 1000)
