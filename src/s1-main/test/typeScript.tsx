0c7c0e1705e0f9495a4332c88651b34e813a3c1a
Artur Khareksian <r2mail1122@gmail.com>
2024-08-12 12:18:37 +0300

    commit

--- src/s1-main/test/typeScript.tsx
+++ src/s1-main/test/typeScript.tsx
@@ -1,82 +1,84 @@
 export const TypeScript = () => {
-  const data = [
-    { id: 1, name: "siberia can code ❄️" },
-    { id: 2, body: { name: "siberia can code ❄️" } },
-    { id: 3, type: "person", name: "siberia", lastname: "can code ❄️" },
-  ]
+  // const data = [
+  //   { id: 1, name: "siberia can code ❄️" },
+  //   { id: 2, body: { name: "siberia can code ❄️" } },
+  //   { id: 3, type: "person", name: "siberia", lastname: "can code ❄️" },
+  // ]
 
-  const flatData = (data: any) => {}
+  // const flatData = (data: any) => {}
 
-  const result = flatData(data)
-  console.log(result)
+  // const result = flatData(data)
+  // console.log(result)
   //   [
   //     { id: 1, name: "siberia can code ❄️" },
   //     { id: 2, name: "siberia can code ❄️" },
   //     { id: 3, name: "siberia can code ❄️"},
   //   ]
 
+  //-----------------------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * 1) Необходимо обойти дерево и вернуть сумму значений
+   * ! Типизировать tree и sumTree
+   ***/
+  // type Tree = {
+  //   v: number
+  //   c?: Tree[]
+  // }
+  // const tree = [
+  //   {
+  //     v: 5,
+  //     c: [
+  //       {
+  //         v: 5,
+  //       },
+  //       {
+  //         v: 10,
+  //         c: [
+  //           {
+  //             v: 11,
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         v: 11,
+  //         c: [
+  //           {
+  //             v: 12,
+  //             c: [
+  //               {
+  //                 v: 5,
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     v: 5,
+  //     c: [
+  //       {
+  //         v: 7,
+  //       },
+  //       {
+  //         v: 12,
+  //         c: [
+  //           {
+  //             v: 11,
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         v: 14,
+  //       },
+  //     ],
+  //   },
+  // ]
+
+  // function sumTree() {  
+  // }
+  // console.log(sumTree(tree)) //108
+
   return <div>TypeScript</div>
 }
-//-----------------------------------------------------------------------------------------------------------------------------------
-
-/**
- * 1) Необходимо обойти дерево и вернуть сумму значений
- * ! Типизировать tree и sumTree
- ***/
-
-const tree = [
-  {
-    v: 5,
-    c: [
-      {
-        v: 5,
-      },
-      {
-        v: 10,
-        c: [
-          {
-            v: 11,
-          },
-        ],
-      },
-      {
-        v: 11,
-        c: [
-          {
-            v: 12,
-            c: [
-              {
-                v: 5,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    v: 5,
-    c: [
-      {
-        v: 7,
-      },
-      {
-        v: 12,
-        c: [
-          {
-            v: 11,
-          },
-        ],
-      },
-      {
-        v: 14,
-      },
-    ],
-  },
-]
-
-function sumTree() {
-
-}
-
-console.log(sumTree(tree)) //108
