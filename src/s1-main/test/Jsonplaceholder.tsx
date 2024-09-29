de7fceea4e75d60ed62df8d5b00ad545b313c614
Artur Khareksian <r2mail1122@gmail.com>
2024-09-29 12:02:24 +0300

    commit

---
+++ src/s1-main/test/Jsonplaceholder.tsx
@@ -0,0 +1,12 @@
+// отобразить список имён с https://jsonplaceholder.typicode.com/users и сделать поиск по имени
+
+export const Jsonplaceholder = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+
