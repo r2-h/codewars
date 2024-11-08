9a9bfcf26cc09d3a118c649b6d80d52ce312ad1a
Artur Khareksian <r2mail1122@gmail.com>
2024-11-08 14:53:03 +0300

    commit

--- src/s1-main/test/useHistory.tsx
+++ src/s1-main/test/useHistory.tsx
@@ -3,19 +3,19 @@ import { useState } from "react"
 const useStateWithHistory = (num) => {}
 
 export function UseHistory() {
-  const [state, setState, goBack, goForward, history] = useStateWithHistory(1)
+  // const [state, setState, goBack, goForward, history] = useStateWithHistory(1)
 
-  return (
-    <div>
-      <div>{state}</div>
-      <div>{history.join(", ")}</div>
-      <button onClick={() => setState(1)}>One</button>
-      <button onClick={() => setState(2)}>Two</button>
-      <button onClick={() => setState(3)}>Three</button>
-      <button onClick={goBack}>goBack</button>
-      <button onClick={goForward}>goForward</button>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <div>{state}</div>
+  //     <div>{history.join(", ")}</div>
+  //     <button onClick={() => setState(1)}>One</button>
+  //     <button onClick={() => setState(2)}>Two</button>
+  //     <button onClick={() => setState(3)}>Three</button>
+  //     <button onClick={goBack}>goBack</button>
+  //     <button onClick={goForward}>goForward</button>
+  //   </div>
+  // )
 }
 /* добавить хук useStateWithHistory, остальное не трогать
 
