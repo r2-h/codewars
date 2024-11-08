d3ea27f59e5d30afda5ca40a81cb8feaa0f0e6fc
Artur Khareksian <r2mail1122@gmail.com>
2024-11-08 14:41:49 +0300

    commit

---
+++ src/s1-main/test/useHistory.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const useStateWithHistory = (num) => {}
+
+export function UseHistory() {
+  const [state, setState, goBack, goForward, history] = useStateWithHistory(1)
+
+  return (
+    <div>
+      <div>{state}</div>
+      <div>{history.join(", ")}</div>
+      <button onClick={() => setState(1)}>One</button>
+      <button onClick={() => setState(2)}>Two</button>
+      <button onClick={() => setState(3)}>Three</button>
+      <button onClick={goBack}>goBack</button>
+      <button onClick={goForward}>goForward</button>
+    </div>
+  )
+}
+/* 
+
+1
+1
+
+2
+1, 2
+
+
+3
+1, 2, 3
+
+2
+1, 2, 3
+
+3
+1, 2, 3
+
+*/
